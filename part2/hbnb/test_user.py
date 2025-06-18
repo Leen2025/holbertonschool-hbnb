e467963352fb0e81018b7408151ede22b922ac3e
Leen2025 <leeeen4@hotmail.com>
2025-06-18 23:42:58 +0000

    __init__.py

--- part2/hbnb/test_user.py
+++ part2/hbnb/test_user.py
@@ -1,11 +1,11 @@
-from models.user import User
+from app.models.user import User
 
 def test_user():
     user = User("Leen", "Alsaleh", "leen@example.com")
     assert user.first_name == "Leen"
     assert user.email == "leen@example.com"
     assert user.is_admin is False
-    print("✅ User test passed")
+    print("User test passed")
 
 if __name__ == "__main__":
     test_user()
