f92ea382713df17ec02c4f7485e8779af1c1277b
Leen2025 <leeeen4@hotmail.com>
2025-06-18 22:09:33 +0000

    reivew

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,26 +1,12 @@
 from app.models.base_model import BaseModel
-from app.models.place import Place
-from app.models.user import User
 
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
-
-        # Validate required text field
         if not text:
-            raise ValueError("Review text is required.")
-
-        # Validate rating between 1 and 5
+            raise ValueError("Review text required")
         if not (1 <= rating <= 5):
-            raise ValueError("Rating must be an integer between 1 and 5.")
-
-        # Validate place is instance of Place
-        if not isinstance(place, Place):
-            raise ValueError("Place must be a Place instance.")
-
-        # Validate user is instance of User
-        if not isinstance(user, User):
-            raise ValueError("User must be a User instance.")
+            raise ValueError("Rating must be between 1 and 5")
 
         self.text = text
         self.rating = rating
