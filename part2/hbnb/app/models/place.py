b9cff31a8d1f1338f591560a4d9852d1369e616c
Leen2025 <leeeen4@hotmail.com>
2025-06-18 18:33:09 +0000

    place.py

---
+++ part2/hbnb/app/models/place.py
@@ -0,0 +1,32 @@
+from app.models.base_model import BaseModel
+from app.models.user import User  # To validate the owner
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+
+        if len(title) > 100:
+            raise ValueError("Title must be 100 characters or fewer.")
+        if price <= 0:
+            raise ValueError("Price must be a positive value.")
+        if not (-90.0 <= latitude <= 90.0):
+            raise ValueError("Latitude must be between -90.0 and 90.0.")
+        if not (-180.0 <= longitude <= 180.0):
+            raise ValueError("Longitude must be between -180.0 and 180.0.")
+        if not isinstance(owner, User):
+            raise ValueError("Owner must be a User instance.")
+
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []   # List to store reviews related to the place
+        self.amenities = [] # List to store amenities related to the place
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        self.amenities.append(amenity)
