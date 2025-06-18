081a96d744a0a159757fab811d14ee05d7c86135
Leen2025 <leeeen4@hotmail.com>
2025-06-18 17:56:38 +0000

    facade.py

---
+++ part2/hbnb/app/services/facade.py
@@ -0,0 +1,17 @@
+from app.persistence.repository import InMemoryRepository
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        #Add the user creation area here later
+        pass
+
+    def get_place(self, place_id):
+        #Add the place fetch area here later
+        pass
+
