361fdf93f7428c7e99692ab6910c253feb0c5d03
Leen2025 <leeeen4@hotmail.com>
2025-06-18 23:17:23 +0000

    review.py

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,10 +1,12 @@
-from app.models.base_model import BaseModel
+# models/review.py
+
+from models.base_model import BaseModel
 
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
         if not text:
-            raise ValueError("Review text required")
+            raise ValueError("Review text is required")
         if not (1 <= rating <= 5):
             raise ValueError("Rating must be between 1 and 5")
 
