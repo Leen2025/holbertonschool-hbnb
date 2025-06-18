3aa9f50aa4beaef4c1736d4fef13587824b4a30b
Leen2025 <leeeen4@hotmail.com>
2025-06-18 23:25:17 +0000

    test

--- part2/hbnb/test_user.py
+++ part2/hbnb/test_user.py
@@ -1,15 +1,11 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from models.user import User
 
 def test_user():
     user = User("Leen", "Alsaleh", "leen@example.com")
     assert user.first_name == "Leen"
+    assert user.last_name == "Alsaleh"
     assert user.email == "leen@example.com"
     assert user.is_admin is False
-    print(" User test passed")
+    print("✅ User test passed")
 
 test_user()
-
