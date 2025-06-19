69ae466a9967f0f6e6aa0804932750ef4e2c607f
d-alshehri <10674@holbertonstudents.com>
2025-06-19 13:52:16 +0000

    Updated facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -23,4 +23,10 @@ class HBnBFacade:
         return self.user_repo.get_all()
 
     def update_user(self, user_id, user_data):
-        return self.user_repo.update(user_id, user_data)
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        for key, value in user_data.items():
+            setattr(user, key, value)
+        self.user_repo.update(user_id, user)
+        return user
