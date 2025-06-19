9da9576e69bac683537b563dd75c718209f48340
d-alshehri <10674@holbertonstudents.com>
2025-06-19 14:16:30 +0000

    Added to facade.py file amenity methods

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -6,7 +6,9 @@ from app.repositories.in_memory_repository import InMemoryRepository
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
+    # --- User methods ---
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -30,3 +32,22 @@ class HBnBFacade:
             setattr(user, key, value)
         self.user_repo.update(user_id, user)
         return user
+
+    # --- Amenity methods ---
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            return None
+        amenity.update(amenity_data)
+        return amenity    
