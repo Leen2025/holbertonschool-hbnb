dd4a07f1ad5f5cfd1b43b4f27ac4d1e7fa8ec0b9
Leen2025 <leeeen4@hotmail.com>
2025-06-19 11:50:52 +0000

    __init__.py

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,15 +1,12 @@
-# app/__init__.py
-
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
+from app.api.v1.users import api as users_ns  # استيراد namespace المستخدمين
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Register user namespace under /api/v1/users
+
     api.add_namespace(users_ns, path='/api/v1/users')
 
     return app
-
