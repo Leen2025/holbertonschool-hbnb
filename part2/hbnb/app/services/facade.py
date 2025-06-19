c7e597e22c5887b02c510e57a9a95a6faf724651
ShAlabbadi <10708@holbertonstudents.com>
2025-06-19 09:36:54 +0000

    update

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,16 +1,33 @@
-from app.persistence.repository import InMemoryRepository
-
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
+    # Existing methods...
     def create_user(self, user_data):
-        
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    def get_place(self, place_id):
-        
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    # New methods to implement
+    def get_all_users(self):
+        """Retrieve all users from the repository"""
+        return self.user_repo.list_all()
+
+    def update_user(self, user_id, user_data):
+        """Update a user's information"""
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+
+        # Update user attributes
+        for key, value in user_data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+
+        return user
--- part2/hbnb/run.py
+++ part2/hbnb/run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from flask import Flask
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def hello():
+    return "Hello World!", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
