585762a8ccddd4712a74557c642ab6d69cdff7cf
ShAlabbadi <10708@holbertonstudents.com>
2025-06-19 09:57:41 +0000

    update4

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -2,7 +2,6 @@ class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
 
-    # Existing methods...
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -14,20 +13,13 @@ class HBnBFacade:
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
 
-    # New methods to implement
     def get_all_users(self):
-        """Retrieve all users from the repository"""
-        return self.user_repo.list_all()
+        return self.user_repo.list()
 
     def update_user(self, user_id, user_data):
-        """Update a user's information"""
         user = self.user_repo.get(user_id)
         if not user:
             return None
-
-        # Update user attributes
         for key, value in user_data.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-
+            setattr(user, key, value)
         return user
