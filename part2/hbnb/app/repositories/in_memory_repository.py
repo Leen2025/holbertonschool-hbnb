e8d244cba38fc4e9713d92c7040f1b8ada9047fb
d-alshehri <10674@holbertonstudents.com>
2025-06-19 13:54:57 +0000

    Updated in_memory_repository.py

--- part2/hbnb/app/repositories/in_memory_repository.py
+++ part2/hbnb/app/repositories/in_memory_repository.py
@@ -4,27 +4,17 @@ import uuid
 
 class InMemoryRepository:
     def __init__(self):
-        # Dictionary to store data: {id: object}
-        self._storage = {}
-
+        self.objects = {}
     def add(self, obj):
-        """Add a new object and assign a unique ID"""
-        if not hasattr(obj, 'id') or not obj.id:
-            obj.id = str(uuid.uuid4())
-        self._storage[obj.id] = obj
-        return obj
-
+        self.objects[obj.id] = obj
     def get(self, obj_id):
-        """Retrieve an object by its ID"""
-        return self._storage.get(obj_id)
-
-    def get_all(self):
-        """Retrieve all stored objects"""
-        return list(self._storage.values())
-
-    def get_by_attribute(self, attr_name, attr_value):
-        """Find an object by a specific attribute value"""
-        for obj in self._storage.values():
-            if hasattr(obj, attr_name) and getattr(obj, attr_name) == attr_value:
+        return self.objects.get(obj_id)
+    def get_by_attribute(self, attr, value):
+        for obj in self.objects.values():
+            if getattr(obj, attr, None) == value:
                 return obj
         return None
+    def get_all(self):
+        return list(self.objects.values())
+    def update(self, obj_id, obj):
+        self.objects[obj_id] = obj
