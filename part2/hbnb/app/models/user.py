cacee586dd238adcb89636a7de94e66c51cf1b12
Leen2025 <leeeen4@hotmail.com>
2025-06-20 23:03:02 +0000

    updaye

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -7,3 +7,12 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "is_admin": self.is_admin
+        }
