5c1b40db472e365e3055399351495030f7da3e3a
Leen2025 <leeeen4@hotmail.com>
2025-06-20 23:57:31 +0000

    update

--- part2/hbnb/app/api/v1/__init__.py
+++ part2/hbnb/app/api/v1/__init__.py
@@ -1,5 +1,13 @@
+from flask_restx import Api
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
+
+api = Api(
+    title='HBnB API',
+    version='1.0',
+    description='API for HBnB project'
+)
 
 api.add_namespace(users_ns)
 api.add_namespace(places_ns)
