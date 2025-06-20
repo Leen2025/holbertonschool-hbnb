44baf805eacfafee9fafa8a887c89561f13162ca
Leen2025 <leeeen4@hotmail.com>
2025-06-20 19:29:49 +0000

    add place

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,10 +1,10 @@
 from app.api.v1.amenities import api as amenities_ns
-
+from app.api.v1.places import api as places_ns
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')  # Add this line
-
+    api.add_namespace(places_ns, path='/api/v1/places')
     return app
