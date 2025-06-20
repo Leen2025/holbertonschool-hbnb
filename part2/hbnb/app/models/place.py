372ec6cf2ee9258defbe2ee16f91374f5f31cbff
Leen2025 <leeeen4@hotmail.com>
2025-06-20 20:21:49 +0000

    place.py

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -6,28 +6,19 @@ class Place(BaseModel):
 
         if not title or len(title) > 100:
             raise ValueError("Title is required and must be less than 100 characters.")
-        if price <= 0:
-            raise ValueError("Price must be a positive number.")
-        if latitude < -90.0 or latitude > 90.0:
-            raise ValueError("Latitude must be between -90.0 and 90.0.")
-        if longitude < -180.0 or longitude > 180.0:
-            raise ValueError("Longitude must be between -180.0 and 180.0.")
         if not owner:
             raise ValueError("Owner (User instance) is required.")
 
         self.title = title
         self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
-        self.owner = owner  # User instance
-        self.reviews = []  # Related reviews
-        self.amenities = []  # Related amenities
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
+        self.price = price           # will call the setter below
+        self.latitude = latitude     # will call the setter below
+        self.longitude = longitude   # will call the setter below
+        self.owner = owner
+        self.reviews = []
+        self.amenities = []
 
-        @property
+    @property
     def price(self):
         return self._price
 
