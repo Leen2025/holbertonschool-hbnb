b5038c4ec6c899ed0b54d12d1f3ec45e55ae23f6
ShAlabbadi <10708@holbertonstudents.com>
2025-06-30 08:21:03 +0000

    task 6

--- part3/app/routes.py
+++ part3/app/routes.py
@@ -1,13 +1,31 @@
-@app.route('/users/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.get_json()
-    updated = facade.update_user(user_id, data)
-    return jsonify({
-        'id': updated.id,
-        'email': updated.email
-    })
+from flask import request, jsonify
+from app import app
+from app.services.facade import HBnBFacade
 
-@app.route('/users/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    facade.delete_user(user_id)
-    return jsonify({'status': 'deleted'}), 204
+facade = HBnBFacade()
+
+@app.route('/api/v1/users/', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    try:
+        user = facade.create_user(data)
+        return jsonify({
+            'id': user.id,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        }), 201
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/api/v1/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = facade.get_user(user_id)
+    if user:
+        return jsonify({
+            'id': user.id,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        })
+    return jsonify({'error': 'User not found'}), 404
