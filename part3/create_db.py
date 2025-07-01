e4a6e83831d3b0052d9231058d7bbe88c7a44d7b
Leen2025 <leeeen4@hotmail.com>
2025-07-01 07:27:49 +0000

    update

--- part3/create_db.py
+++ part3/create_db.py
@@ -1,7 +1,15 @@
-from app import create_app
 from app.extensions import db
+from app import create_app
+
+
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
+from app.models.place_amenity import place_amenities
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+    print("✅ Tables created successfully!")
