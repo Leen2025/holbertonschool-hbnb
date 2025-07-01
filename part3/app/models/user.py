9db9e8af8dd7819e2dc470f28cd677ef01880b57
Leen2025 <leeeen4@hotmail.com>
2025-07-01 07:59:32 +0000

    update

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -9,6 +9,7 @@ class User(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+     is_admin = db.Column(db.Boolean, default=False)
 
     places = db.relationship('Place', back_populates='owner', cascade='all, delete-orphan')
     reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
@@ -25,6 +26,7 @@ class User(db.Model):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email": self.email
+            "is_admin": self.is_admin
             }
 
 from app.models.place import Place
