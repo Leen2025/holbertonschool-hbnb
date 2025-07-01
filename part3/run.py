a9b483e5724f8a80f17912bdfb3b3cbc9f3c29a8
d-alshehri <10674@holbertonstudents.com>
2025-07-01 09:16:12 +0000

    Edited run file

--- part3/run.py
+++ part3/run.py
@@ -1,6 +1,13 @@
+# run.py
+
 from app import create_app
+from app.extensions import db
+from app.models import user, place, amenity, review  # make sure all models are imported
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run()
+with app.app_context():
+    db.create_all()  # This creates all tables if they don't exist
+
+if __name__ == '__main__':
+    app.run(debug=True)
