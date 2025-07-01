2b7f095e198948ee8f2275b52cb69138b2aec4b2
d-alshehri <10674@holbertonstudents.com>
2025-07-01 10:24:24 +0000

    Create user file

---
+++ part3/create_user.py
@@ -0,0 +1,17 @@
+from app import db, create_app
+from app.models.user import User
+
+app = create_app()
+app.app_context().push()
+
+# Create a test user
+user = User(
+    id="user-100",
+    email="danah@example.com",
+    password="danah12345",  # If you use password hashing, hash this!
+    first_name="Danah",
+    last_name="Alshehri"
+)
+db.session.add(user)
+db.session.commit()
+print("User created.")
